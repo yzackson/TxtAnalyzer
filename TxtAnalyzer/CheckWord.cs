c703082aeb431f072566a844d1a425c324ec0bc3
Isaac Pires <isaac.aragao.p@gmail.com>
2022-06-14 17:09:07 -0300

    Initial commit

---
+++ TxtAnalyzer/CheckWord.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TxtAnalyzer
+{
+    class CheckWord
+    {
+        private String[] AllText;
+        private Hashtable Dictionary = new Hashtable();
+        public String DictPath;
+
+        // Event to get new dict
+        public event EventHandler<String> GetNewDict;
+
+        // set path to new
+        public void SetDictPath(String path)
+        {
+            if (DictPath != path)
+            {
+                DictPath = path;
+                OnDictPathChenged();
+            }
+        }
+
+        // Method to be called when path change
+        protected virtual void OnDictPathChenged()
+        {
+            GetNewDict?.Invoke(this, this.DictPath);
+        }
+
+        // Import new dictionary
+        public void UpdateDictionary(object sender, String path)
+        {
+            String _dict = File.ReadAllText(path, Encoding.UTF8);
+            foreach (string word in _dict.Split(','))
+            {
+                Dictionary.Add(word.ToLower(), true);
+            }
+        }
+
+        // Set the current text took of the textbox
+        public async Task SetAllText(TextBox text)
+        {
+            Task<String[]> Text = Task.Run(() =>
+            {
+                return text.Text
+                    .ToLower()
+                    .Split(new string[] { " ", "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries);
+            });
+
+            await Text.ContinueWith(t =>
+            {
+                AllText = t.Result;
+                CheckAllTextInDict();
+            });
+        }
+
+        // Check if all the text got of the textbox is in dictionary
+        private async Task CheckAllTextInDict()
+        {
+            await Task.Run(() =>
+            {
+                foreach (String word in AllText)
+                {
+                    if (!Dictionary.Contains(word))
+                    {
+                        Dictionary.Add(word, false);
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ TxtAnalyzer/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TxtAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        //private String[] originalText = null;
+        internal Hashtable _dictionary = new Hashtable();
+        CheckWord CheckWord = new CheckWord();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            CheckWord.GetNewDict += CheckWord.UpdateDictionary;
+
+            CheckWord.SetDictPath(@"C:\Dev\Faculdade\TxtAnalyzer\TxtAnalyzer\dicionario.txt");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox.Text.EndsWith(" ") || textBox.Text.EndsWith("\n"))
+            {
+                CheckWord.SetAllText(textBox);
+            }
+        }
+    }
+}
